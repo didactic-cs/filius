4a0bac9c00a9cb558b421039f26c0500fc7013d5
Patrice Tréton <patrice.treton@ac-normandie.fr>
2020-06-17 15:37:28 +0200

    New version of the FileExplorer. FiliusFileSystem Updated (2)

---
+++ src/main/java/filius/gui/anwendungssicht/GUIApplicationFileExplorerImageViewer.java
@@ -0,0 +1,44 @@
+package filius.gui.anwendungssicht;
+
+import java.awt.Dimension;
+
+import javax.swing.JInternalFrame;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class GUIApplicationFileExplorerImageViewer extends JInternalFrame {
+	
+	private static GUIApplicationFileExplorerImageViewer singleton = null;	
+	private GUIDesktopPanel desktop = null;  //JBackgroundDesktopPane
+	
+	public GUIApplicationFileExplorerImageViewer() {
+        super();
+        this.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
+
+        this.setPreferredSize(new Dimension(400, 300));
+        this.setClosable(true);
+        this.setMaximizable(true);
+        this.setIconifiable(false);
+        this.setResizable(true);
+        
+        this.setTitle("Preview");
+    }
+
+    /*
+	 * returns the instance of the singleton
+	 */
+	public static GUIApplicationFileExplorerImageViewer gI() {
+		if (singleton == null) {
+			singleton = new GUIApplicationFileExplorerImageViewer();
+		}
+		return singleton;
+	}
+	
+	public void preview(JInternalFrame launcher, DefaultMutableTreeNode node) {
+//		if (desktop == null) {
+//			desktop = (GUIDesktopPanel) launcher.getParent().getParent();
+//			desktop.add(this);
+//		}
+//		this.show();
+	}
+
+}
