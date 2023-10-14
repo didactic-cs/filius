ff0d049aa90df9435e85c283855e632c2e594d14
Stefan Freischlad <stefan@lernsoftware-filius.de>
2023-10-14 22:10:38 +0200

    fix host config dialog

--- src/main/java/filius/gui/netzwerksicht/JHostKonfiguration.java
+++ src/main/java/filius/gui/netzwerksicht/JHostKonfiguration.java
@@ -529,13 +529,6 @@ public class JHostKonfiguration extends JKonfiguration implements I18n {
 
             Betriebssystem bs = (Betriebssystem) host.getSystemSoftware();
 
-            useLAN.setSelected(!bs.wireless());
-            useWiFi.setSelected(bs.wireless());
-            useWiFi.setEnabled(host.holeFreienPort() != null || bs.wireless());
-            ssid.setEnabled(useWiFi.isSelected());
-            ssidLabel.setEnabled(ssid.isEnabled());
-            updateSsid();
-
             macAdresse.setText(bs.primaryMACAddress());
             ipAdresse.setText(bs.primaryIPAdresse());
             netzmaske.setText(bs.primarySubnetMask());
@@ -550,6 +543,13 @@ public class JHostKonfiguration extends JKonfiguration implements I18n {
             netzmaske.setEnabled(!bs.isDHCPKonfiguration());
             gateway.setEnabled(!bs.isDHCPKonfiguration());
             dns.setEnabled(!bs.isDHCPKonfiguration());
+
+            useLAN.setSelected(!bs.wireless());
+            useWiFi.setSelected(bs.wireless());
+            useWiFi.setEnabled(host.holeFreienPort() != null || bs.wireless());
+            ssid.setEnabled(useWiFi.isSelected());
+            ssidLabel.setEnabled(ssid.isEnabled());
+            updateSsid();
 
             checkIpAddress();
             checkDnsAddress();
