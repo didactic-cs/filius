d887788048aef235592d11e8e6ed5117c6c24099
Matthias Brauer <ma.brauer@ostfalia.de>
2023-11-28 09:35:32 +0000

    Die Methode verarbeiteDatenEnheit überrüft jetzt auch, ob ein Eintrag in der NAT-Tabelle vorliegt

--- src/main/java/filius/software/nat/NatGatewayWANThread.java
+++ src/main/java/filius/software/nat/NatGatewayWANThread.java
@@ -50,7 +50,12 @@ public class NatGatewayWANThread extends FirewallThread implements I18n {
     @Override
     protected void verarbeiteDatenEinheit(EthernetFrame frame) {
         Lauscher.getLauscher().addDatenEinheit(wanNic.getMac(), frame);
-        if (!checkToLANAddress(frame) && !checkDiscardByFirewall(frame)) {
+        boolean check = true;
+        if (frame.getDaten() instanceof IpPaket) {
+            IpPaket packet = (IpPaket) frame.getDaten();
+            check = (natGateway.eintragExistiert(packet));
+        }
+        if (!checkToLANAddress(frame) && !checkDiscardByFirewall(frame) && check) {
             modifyIncomingFrame(frame);
             forwardFrame(frame);
         }
