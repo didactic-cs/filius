64d9e3d4f7391a1388d49f1696149b8af235d23c
Matthias Brauer <ma.brauer@ostfalia.de>
2023-11-28 09:43:36 +0000

    Wurde massiv erweitert, damit die NAT-Tabelle richtig arbeiten kann

--- src/main/java/filius/software/nat/NetworkAddressTranslationTable.java
+++ src/main/java/filius/software/nat/NetworkAddressTranslationTable.java
@@ -26,9 +26,11 @@
 package filius.software.nat;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,33 +38,86 @@ import org.slf4j.LoggerFactory;
 public class NetworkAddressTranslationTable {
     private static Logger LOG = LoggerFactory.getLogger(NetworkAddressTranslationTable.class);
 
-    private Map<PortProtocolPair, InetAddress> dynamicNATTable = new HashMap<>();
-    private Map<PortProtocolPair, InetAddress> staticNATTable = new HashMap<>();
+    private ConcurrentHashMap<PortProtocolPair, NatEntry> dynamicNATTable = new ConcurrentHashMap<>();
+    private Map<PortProtocolPair, NatEntry> staticNATTable = new HashMap<>();
+    private NatMethod natMethod = NatMethod.fullCone;
 
-    public Map<PortProtocolPair, InetAddress> getStaticNATTable() {
+    public Map<PortProtocolPair, NatEntry> getStaticNATTable() {
         return Collections.unmodifiableMap(staticNATTable);
     }
 
-    public void setStaticNATTable(Map<PortProtocolPair, InetAddress> staticNATTable) {
-        this.staticNATTable = new HashMap<PortProtocolPair, InetAddress>(staticNATTable);
+    public ConcurrentHashMap<PortProtocolPair, NatEntry> getDynamicNATTable() {
+        return dynamicNATTable;
     }
 
-    void addDynamic(int port, int protocol, InetAddress address) {
-        dynamicNATTable.put(new PortProtocolPair(port, protocol), address);
+    public void setStaticNATTable(Map<PortProtocolPair, NatEntry> staticNATTable) {
+        this.staticNATTable = new HashMap<PortProtocolPair, NatEntry>(staticNATTable);
+    }
+    
+    public void resetStaticNATTable() {
+    	this.staticNATTable.clear();
     }
 
-    public InetAddress find(int port, int protocol) {
-        PortProtocolPair lookup = new PortProtocolPair(port, protocol);
-        return dynamicNATTable.get(lookup);
+    void addDynamic(int port, String wanAddress, int protocol, InetAddress lanAddress, NatType natType) {
+    	if (lanAddress != null) {
+    		NatEntry entry = new NatEntry();
+    		entry.setInetAddress(lanAddress);
+    		entry.setNatType(natType);
+    		entry.setLastUpdate(new Date());
+    		dynamicNATTable.put(new PortProtocolPair(port, (natMethod == NatMethod.restrictedCone?wanAddress:""), protocol), entry);
+        }
+    }
+
+    public void addStatic(int port, int protocol, InetAddress lanAddress) {
+    	if (lanAddress != null) {
+    		NatEntry entry = new NatEntry();
+    		entry.setInetAddress(lanAddress);
+    		entry.setNatType(NatType.StaticEntry);
+    		staticNATTable.put(new PortProtocolPair(port, "", protocol), entry);
+    	}
+    }
+    
+    public NatMethod getNatMethod() {
+    	return natMethod;
+    }
+    
+    public void setNatMethod(NatMethod method) {
+    	this.natMethod = method;
+    }
+
+    public InetAddress find(int port, String address, int protocol) {
+        PortProtocolPair lookup = new PortProtocolPair(port,(natMethod == NatMethod.restrictedCone?address:""), protocol);
+        NatEntry natEntry = dynamicNATTable.get(lookup);
+        InetAddress IpAddress = null;
+        if (natEntry == null) {
+        	lookup = new PortProtocolPair(port, "", protocol);
+            natEntry = staticNATTable.get(lookup);
+        	if (natEntry!= null) {
+            	IpAddress = natEntry.getInetAddress();
+            	addDynamic(port,address, protocol, IpAddress, NatType.DynamicEnryFromStatic);
+        	}
+        } else {
+        	natEntry.setLastUpdate(new Date());
+    		IpAddress = natEntry.getInetAddress();
+    	}
+        return IpAddress;
     }
 
     public boolean hasConnection(InetAddress lanAddress) {
         boolean exists = false;
         for (PortProtocolPair key : dynamicNATTable.keySet()) {
-            if (dynamicNATTable.get(key).equals(lanAddress)) {
+            if (natMethod == NatMethod.restrictedCone?dynamicNATTable.get(key).getInetAddress().equals(lanAddress):dynamicNATTable.get(key).getInetAddress().equalsWOIp(lanAddress)) {
                 exists = true;
                 break;
             }
+        }
+        if (!exists) {
+        	for (PortProtocolPair key : staticNATTable.keySet()) {
+            	if (staticNATTable.get(key).getInetAddress().equalsWOIp(lanAddress)) {
+                	exists = true;
+                	break;
+            	}
+        	}
         }
         return exists;
     }
@@ -70,17 +125,28 @@ public class NetworkAddressTranslationTable {
     public int findPort(InetAddress lanAddress) {
         int port = 0;
         for (PortProtocolPair key : dynamicNATTable.keySet()) {
-            if (dynamicNATTable.get(key).equals(lanAddress)) {
+            if (natMethod == NatMethod.restrictedCone?dynamicNATTable.get(key).getInetAddress().equals(lanAddress):dynamicNATTable.get(key).getInetAddress().equalsWOIp(lanAddress)) {
                 port = key.getPort();
                 break;
             }
+        }
+        if (port == 0) {
+        	for (PortProtocolPair key : staticNATTable.keySet()) {
+        		if (staticNATTable.get(key).getInetAddress().equalsWOIp(lanAddress)) {
+        			port = key.getPort();
+        			break;
+        		}
+        	}
         }
         return port;
     }
 
     public void print() {
-        for (Entry<PortProtocolPair, InetAddress> entry : dynamicNATTable.entrySet()) {
-            LOG.debug("{} -> {}", entry.getKey(), entry.getValue());
+        for (Entry<PortProtocolPair, NatEntry> natEntry : staticNATTable.entrySet()) {
+            LOG.debug("{} -> {}", natEntry.getKey(), natEntry.getValue());
+        }
+        for (Entry<PortProtocolPair, NatEntry> natEntry : dynamicNATTable.entrySet()) {
+            LOG.debug("{} -> {}", natEntry.getKey(), natEntry.getValue());
         }
     }
 }
