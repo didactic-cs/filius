78a17079600e8eebb6a69dd41774cfcbb5b88cfb
Matthias Brauer <ma.brauer@ostfalia.de>
2023-11-28 10:27:50 +0000

    Erweitert, da es derungen an der Klasse NetwporkAddressTranslatinTable gab

--- src/test/java/filius/software/nat/NetworkAddressTranslationTableTest.java
+++ src/test/java/filius/software/nat/NetworkAddressTranslationTableTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 
 import filius.software.vermittlungsschicht.IpPaket;
 
-public class NetworkAddressTranslationTableTest {
+public class NetworkAddressTranslationTableTest { //neu
     private NetworkAddressTranslationTable table = new NetworkAddressTranslationTable();
 
     @Test
     public void testFind_EmptyTable_Null() throws Exception {
-        assertNull(table.find(1, IpPaket.TCP));
+        assertNull(table.find(1, "", IpPaket.TCP));
     }
 
     @Test
     public void testFind_EntryExists() throws Exception {
         InetAddress address = new InetAddress("1.1.1.1", 2002, IpPaket.TCP);
-        table.addDynamic(1, IpPaket.TCP, address);
+        table.addDynamic(1, "192.168.0.1", IpPaket.TCP, address, NatType.DynamicEntry);
 
-        assertEquals(address, table.find(1, IpPaket.TCP));
+        assertEquals(address, table.find(1, "192.168.0.1", IpPaket.TCP));
     }
 }
