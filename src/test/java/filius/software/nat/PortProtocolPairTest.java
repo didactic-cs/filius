3c4de35bacd954c2bc11d7127816cdb7bc73c51b
Matthias Brauer <ma.brauer@ostfalia.de>
2023-11-28 10:29:48 +0000

    Erweitert, da es Änderungen in der Klasse PortProtocolPair gab

--- src/test/java/filius/software/nat/PortProtocolPairTest.java
+++ src/test/java/filius/software/nat/PortProtocolPairTest.java
@@ -7,40 +7,40 @@ import org.junit.Test;
 
 import filius.software.vermittlungsschicht.IpPaket;
 
-public class PortProtocolPairTest {
+public class PortProtocolPairTest { //neu
 
     @Test
     public void testEquals_True() throws Exception {
-        PortProtocolPair pair = new PortProtocolPair(5555, IpPaket.TCP);
+        PortProtocolPair pair = new PortProtocolPair(5555, "192.168.0.1", IpPaket.TCP);
 
-        assertTrue(pair.equals(new PortProtocolPair(pair.getPort(), pair.getProtocol())));
+        assertTrue(pair.equals(new PortProtocolPair(pair.getPort(), "192.168.0.1", pair.getProtocol())));
     }
 
     @Test
     public void testEquals_Null_False() throws Exception {
-        PortProtocolPair pair = new PortProtocolPair(5555, IpPaket.TCP);
+        PortProtocolPair pair = new PortProtocolPair(5555, "192.168.0.1", IpPaket.TCP);
 
         assertFalse(pair.equals(null));
     }
 
     @Test
     public void testEquals_NOPortProtocolPair_False() throws Exception {
-        PortProtocolPair pair = new PortProtocolPair(5555, IpPaket.TCP);
+        PortProtocolPair pair = new PortProtocolPair(5555, "192.168.0.1", IpPaket.TCP);
 
         assertFalse(pair.equals("hallo"));
     }
 
     @Test
     public void testEquals_DifferentPort_False() throws Exception {
-        PortProtocolPair pair = new PortProtocolPair(5555, IpPaket.TCP);
+        PortProtocolPair pair = new PortProtocolPair(5555, "192.168.0.1", IpPaket.TCP);
 
-        assertFalse(pair.equals(new PortProtocolPair(pair.getPort() + 100, pair.getProtocol())));
+        assertFalse(pair.equals(new PortProtocolPair(pair.getPort() + 100, "192.168.0.1", pair.getProtocol())));
     }
 
     @Test
     public void testEquals_DifferentProtocol_False() throws Exception {
-        PortProtocolPair pair = new PortProtocolPair(5555, IpPaket.TCP);
+        PortProtocolPair pair = new PortProtocolPair(5555, "192.168.0.1", IpPaket.TCP);
 
-        assertFalse(pair.equals(new PortProtocolPair(pair.getPort(), pair.getProtocol() + 100)));
+        assertFalse(pair.equals(new PortProtocolPair(pair.getPort(), "192.168.0.1", pair.getProtocol() + 100)));
     }
 }
