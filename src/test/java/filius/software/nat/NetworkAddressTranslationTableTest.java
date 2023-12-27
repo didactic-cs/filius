fcca848cd2812746217904f2e8a4342e649823b7
Stefan Freischlad <stefan@lernsoftware-filius.de>
2023-12-27 19:16:30 +0100

    Revert "Erweitert, da es derungen an der Klasse NetwporkAddressTranslatinTable gab"
    This reverts commit 3e9b1165e91a3ec9b877f13121bc3d653f9e8d92.

--- src/test/java/filius/software/nat/NetworkAddressTranslationTableTest.java
+++ src/test/java/filius/software/nat/NetworkAddressTranslationTableTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 
 import filius.software.vermittlungsschicht.IpPaket;
 
-public class NetworkAddressTranslationTableTest { //neu
+public class NetworkAddressTranslationTableTest {
     private NetworkAddressTranslationTable table = new NetworkAddressTranslationTable();
 
     @Test
     public void testFind_EmptyTable_Null() throws Exception {
-        assertNull(table.find(1, "", IpPaket.TCP));
+        assertNull(table.find(1, IpPaket.TCP));
     }
 
     @Test
     public void testFind_EntryExists() throws Exception {
         InetAddress address = new InetAddress("1.1.1.1", 2002, IpPaket.TCP);
-        table.addDynamic(1, "192.168.0.1", IpPaket.TCP, address, NatType.DynamicEntry);
+        table.addDynamic(1, IpPaket.TCP, address);
 
-        assertEquals(address, table.find(1, "192.168.0.1", IpPaket.TCP));
+        assertEquals(address, table.find(1, IpPaket.TCP));
     }
 }
